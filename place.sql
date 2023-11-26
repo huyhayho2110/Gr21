ac97f7a49ec8b7d3e417723f79b66c5511543798
BatHackman <119168962+onedayfirst@users.noreply.github.com>
2023-11-26 22:26:22 +0700

    Add files via upload

---
+++ place.sql
@@ -0,0 +1,33 @@
+CREATE TABLE place_result (
+  id INT auto_increment,
+  place INT,
+  ID_Teams INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (ID_Teams) REFERENCES Teams(ID_Teams)  
+);
+
+INSERT INTO place_result (place, ID_Teams) VALUES
+(1,8),
+(2,5),
+(3,2),
+(4,6),
+(5,10),
+(6,4),
+(7,7),
+(8,3),
+(9,11),
+(10,1),
+(11,15),
+(12,9),
+(13,13),
+(14,18),
+(15,14),
+(16,17),
+(17,12),
+(18,23),
+(19,16),
+(20,22),
+(21,19),
+(22,20),
+(23,24),
+(24,21);
