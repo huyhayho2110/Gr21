54dd5b291f111673c1872e29a53cfdbddf26b0b4
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-26 21:52:13 +0700

    Update Tourament.sql

--- Tourament.sql
+++ Tourament.sql
@@ -2,8 +2,11 @@ CREATE TABLE Tourament(
   Name_tourament VARCHAR (255) PRIMARY KEY,
   Date_start DATE NOT NULL,
   Date_end DATE NOT NULL,
-  Location VARCHAR (50)
+  Location VARCHAR (50),
+  Total_prize INT,
+  Unit_money VARCHAR (50),
+  INDEX idx_Unit_money (Unit_money)
 );
 
-INSERT INTO (Name_tourament, Total_prizre, Date_start, Date_end, Location)
-VALUES ('League of legends: Worlds championship 2022', '2022-09-30', '2022-11-06', 'New York');
+INSERT INTO Tourament (Name_tourament, Date_start, Date_end, Location, Total_prize, Unit_money)
+VALUES ('League of legends: Worlds championship 2022', '2022-09-30', '2022-11-06', 'New York', 2225000, 'USD');
