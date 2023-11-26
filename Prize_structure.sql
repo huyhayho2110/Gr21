c221885ffaf2c6e993bba6f3540c5785168e75af
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-26 21:09:16 +0700

    Update Prize_structure.sql

--- Prize_structure.sql
+++ Prize_structure.sql
@@ -1,3 +1,14 @@
 CREATE TABLE Prize_structure (
-  
+  Name_tourament VARCHAR (255),
+  Ranking INT,
+  ID_teams INT,
+  Name_teams VARCHAR (50),
+  Prize_money DECIMAL (10,2),
+  Prize_percentage DECIMAL (4,2),
+  FOREIGN KEY (Name_tourament) REFERENCES Tourament(Name_tourament),
+  FOREIGN KEY (ID_teams) REFERENCES Teams(ID_Teams)
 );
+
+INSERT INTO Prize_structure (Ranking, Name_teams, Prize_money, Prizr_percentage)
+VALUES (1, 'DRX', '489500', '2')
+;
