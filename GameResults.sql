b04de297e657d33db45f7f749ba9c82ac5e0ce70
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-28 22:37:25 +0700

    Update GameResults.sql

--- GameResults.sql
+++ GameResults.sql
@@ -1,13 +1,14 @@
-create table GameResults (
-ID_result INT auto_increment PRIMARY KEY,
-ID_match INT,
-ID_team_winning INT,
-ID_team_losing INT,
-Winning_score INT,
-Losing_score INT,
-FOREIGN KEY (ID_match) REFERENCES Matches(ID_match),
-FOREIGN KEY (ID_team_winning) REFERENCES Teams(ID_Teams),
-FOREIGN KEY (ID_team_losing) REFERENCES Teams(ID_Teams),
+CREATE TABLE GameResults (
+  ID_result INT AUTO_INCREMENT PRIMARY KEY,
+  ID_match INT UNSIGNED,
+  ID_team_winning INT UNSIGNED,
+  ID_team_losing INT UNSIGNED,
+  Winning_score INT,
+  Losing_score INT,
+  FOREIGN KEY (ID_match) REFERENCES Matches(ID_match),
+  FOREIGN KEY (ID_team_winning) REFERENCES Teams_information(Team_ID),
+  FOREIGN KEY (ID_team_losing) REFERENCES Teams_information(Team_ID)
+) COMMENT='Table storing game results.';
 
 INSERT INTO GameResults (ID_result, ID_match, ID_team_winning, ID_team_losing, Winning_score, Losing_score)
 values
@@ -23,7 +24,6 @@ values
 (10, 3, 11, 1, 0),
 (11, 5, 14, 1, 0),
 (12, 5, 3, 1, 0),
-
 (13, 7, 9, 1, 0),
 (14, 2, 5, 1, 0),
 (15, 9, 15, 1, 0),
@@ -36,7 +36,6 @@ values
 (22, 2, 9, 1, 0),
 (23, 7, 15, 1, 0),
 (24, 7, 2, 1, 0),
-
 (25, 10, 8, 1, 0),
 (26, 1, 17, 1, 0),
 (27, 10, 17, 1, 0),
@@ -49,7 +48,6 @@ values
 (34, 8, 17, 1, 0),
 (35, 1, 10, 1, 0),
 (36, 1, 8, 1, 0),
-
 (37, 18, 13, 1, 0),
 (38, 4, 6, 1, 0),
 (39, 6, 13, 1, 0),
@@ -62,13 +60,10 @@ values
 (46, 6, 13, 1, 0),
 (47, 4, 18, 1, 0),
 (48, 6, 4, 1, 0),
-
 (49, 2, 10, 3, 0),
 (50, 5, 4, 3, 0),
 (51, 6, 7, 3, 2),
 (52, 8, 3, 3, 2),
-
 (53, 5, 2, 3, 1),
 (54, 8, 6, 3, 1),
-
 (55, 8, 5, 3, 2);
