20a48897e8354a8831621b1581b1433a09d5c49c
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-28 15:38:46 +0700

    Update Players_information.sql

--- Players_information.sql
+++ Players_information.sql
@@ -1,1 +1,7 @@
-CREATE 
+CREATE TABLE Players_information (
+  ID_players INT PRIMARY KEY,
+  Name_teams VARCHAR(50) NOT NULL,
+  Name_players VARCHAR (50) NOT NULL,
+  Nick_name VARCHAR (50) NOT NULL,
+  
+);
