8d562cc4478dc3624b17b4ff07690dcafeadf0a8
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-28 21:15:27 +0700

    Update Tourament.sql

--- Tourament.sql
+++ Tourament.sql
@@ -1,20 +1,20 @@
-DROP database IF EXISTS lol_2022;
+DROP DATABASE IF EXISTS lol_2022;
 
-CREATE database IF NOT EXISTS lol_2022;
+CREATE DATABASE IF NOT EXISTS lol_2022;
 
 USE lol_2022;
 
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE Tourament(
-  Name_tourament VARCHAR (255) PRIMARY KEY,
+CREATE TABLE Tournament (
+  Tournament_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Name_tournament VARCHAR(255) UNIQUE NOT NULL,
   Date_start DATE NOT NULL,
   Date_end DATE NOT NULL,
-  Location VARCHAR (50),
-  Total_prize INT,
-  Unit_money VARCHAR (50),
-  INDEX idx_Unit_money (Unit_money)
-);
+  Location VARCHAR(255) NOT NULL,
+  Total_prize INT NOT NULL,
+  Unit_money ENUM('USD') NOT NULL
+) COMMENT='Table storing information about tournaments.';
 
-INSERT INTO Tourament (Name_tourament, Date_start, Date_end, Location, Total_prize, Unit_money)
+INSERT INTO Tournament (Name_tournament, Date_start, Date_end, Location, Total_prize, Unit_money)
 VALUES ('League of legends: Worlds championship 2022', '2022-09-30', '2022-11-06', 'New York', 2225000, 'USD');
