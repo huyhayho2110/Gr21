2392f300dd570bedb6ca27f2145e5f0be8d94283
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-28 21:17:22 +0700

    Update Prize_structure.sql

--- Prize_structure.sql
+++ Prize_structure.sql
@@ -1,10 +1,13 @@
 CREATE TABLE Prize_structure (
+  Prize_structure_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Tournament_ID INT UNSIGNED,
   Ranking INT NOT NULL,
-  Prize_money DECIMAL (10,4) NOT NULL,
-  Prize_percentage DECIMAL (4,2) NOT NULL,
-  Unit_money VARCHAR (50) NOT NULL,
-  FOREIGN KEY (Unit_money) REFERENCES Tourament(Unit_money)
-);
+  Prize_money DECIMAL(10,4) NOT NULL,
+  Prize_percentage DECIMAL(4,2) NOT NULL,
+  Unit_money ENUM('USD') NOT NULL,
+  FOREIGN KEY (Tournament_ID) REFERENCES Tournament(Tournament_ID),
+  CHECK (Prize_money >= 0 AND Prize_percentage >= 0 AND Prize_percentage <= 100)
+) COMMENT='Table storing prize structure for tournaments.';
 
 INSERT INTO Prize_structure (Ranking, Prize_money, Prize_percentage, Unit_money)
 VALUES 
