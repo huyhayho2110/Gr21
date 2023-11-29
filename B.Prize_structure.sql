1cdf59152b40481918dec6267e49c349c717ac7e
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 13:26:56 +0700

    Update B.Prize_structure.sql

--- B.Prize_structure.sql
+++ B.Prize_structure.sql
@@ -5,6 +5,6 @@ CREATE TABLE Prize_structure (
   Prize_percentage DECIMAL(4,2) NOT NULL,
   Unit_money ENUM('USD') NOT NULL,
   Location VARCHAR(255) NOT NULL,
-  FOREIGN KEY (Location) REFERENCES Tournament(Location),
+  FOREIGN KEY (Unit_money) REFERENCES Tournament(Unit_money),
   CHECK (Prize_money >= 0 AND Prize_percentage >= 0 AND Prize_percentage <= 100)
 ) COMMENT='Table storing prize structure for tournaments.';
