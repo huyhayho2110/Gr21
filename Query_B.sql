6b1971a431ebed4a73a47f94e3815466ec9f4cfe
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 15:55:17 +0700

    Create Query_B.sql

---
+++ Query_B.sql
@@ -0,0 +1,10 @@
+SELECT
+    ps.Ranking,
+    ps.Prize_money,
+    ps.Prize_percentage
+FROM
+    Prize_structure ps
+ORDER BY
+    ps.Ranking;
+
+-- in ra số tiền thưởng, phần trăm ứng với bậc xếp hạng
