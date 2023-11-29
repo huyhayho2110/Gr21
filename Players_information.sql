b4fbbef3d9a1e93dc51a6b47735586ee1f07ffdf
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 09:46:28 +0700

    Update Players_information.sql

--- Players_information.sql
+++ Players_information.sql
@@ -3,7 +3,9 @@ CREATE TABLE Players_information (
   Name_team VARCHAR(50) NOT NULL,
   Name_player VARCHAR (255) NOT NULL,
   Position_player ENUM('Top','Jungle','Middle','ADC','Support'),
+  Coach_ID INT UNSIGNED,
   FOREIGN KEY (Name_team) REFERENCES Teams_information(Name_team),
+  FOREIGN KEY (Coach_ID) REFERENCES Coaches(Coach_ID) ON DELETE SET NULL,
   CONSTRAINT UQ_Team_Player UNIQUE (Player_ID, Name_player)
 ) COMMENT='Table storing information about players.';
 
