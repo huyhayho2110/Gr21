86e4ea8bea6685696e23537c77ceab141c31cb77
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 11:54:59 +0700

    Update I.Coaches.sql

--- I.Coaches.sql
+++ I.Coaches.sql
@@ -7,30 +7,5 @@ CREATE TABLE Coaches (
   CONSTRAINT UQ_Team_Coach UNIQUE (Team_ID, Coach_name)
 ) COMMENT='Table storing information about coaches.';
 
-INSERT INTO Coaches (Coach_name, Team_ID, nick_name) VALUES
-('Jang Nu-ri', 13, 'Cain'),
-('Nick De Cesare', 14, 'LS'),
-('Chen Chen-Chi', 18, 'Achie'),
-('Kim Jeong-soo', 8, 'Kim'),
-('Yang Dae-In', 7, 'Daeny'),
-('Yang Ji-Song', 3, 'Maokai'),
-('Gabriel Peixoto', 15, 'Turtle'),
-('Joey Steltenpool', 11, 'YoungBuck'),
-('Dylan Falco', 9, 'Dylan Falco'),
-('Phan Huy Phong', 17, 'Jakeywind'),
-('Go Dong-bin', 6, 'Score'),
-('Yoon Sung-young', 2, 'Homme'),
-('Simon Payne', 10, 'Fredy122'),
-('Tan Qi', 4, 'Martin'),
-('Bae Seong-woong', 5, 'Bengi'),
-('Li Guo-Peng1', 1, 'Dian'),
-('James MacCormack', 12, 'Mac'),
-('Nguyen Van Trong', 16, 'Ren'),
-('Li Hsien-Ming', 19, 'Butter'),
-('Markus Leuemberger', 20, 'Ukkyr'),
-('Ali Aklan', 21, 'Craft1x'),
-('Daniel Montaner', 22, 'Montaner'),
-('Han Gi-hun', 23, 'viviD'),
-('Daniel Le', 24, 'Dragku');
 
 
