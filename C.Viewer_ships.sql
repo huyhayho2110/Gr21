44e68b3aa9663a904d5a45bc80ef55bba28797ea
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 14:15:15 +0700

    Update C.Viewer_ships.sql

--- C.Viewer_ships.sql
+++ C.Viewer_ships.sql
@@ -1,9 +1,10 @@
 CREATE TABLE Peek_viewership (
-  Name_tournament VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
+  Peek_viewership_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Name_tournament VARCHAR(255) UNIQUE NOT NULL ,
   Peek_viewers INT NOT NULL,
-  View_english_streams INT UNIQUE NOT NULL,
-  View_twitch_streams INT UNIQUE NOT NULL,
-  Average_viewers INT UNIQUE NOT NULL,
-  Hours_watched INT UNIQUE NOT NULL,
+  View_english_streams INT NOT NULL,
+  View_twitch_streams INT NOT NULL,
+  Average_viewers INT NOT NULL,
+  Hours_watched INT NOT NULL,
   FOREIGN KEY (Name_tournament) REFERENCES Tournament(Name_tournament)
 ) COMMENT='Table storing viewership information across platforms.';
