4b0c59f50ac9632e5d28054b257989a9fed5e9c8
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 13:14:27 +0700

    Update C.Viewer_ships.sql

--- C.Viewer_ships.sql
+++ C.Viewer_ships.sql
@@ -7,7 +7,3 @@ CREATE TABLE Peek_viewership (
   Hours_watched VARCHAR(50) UNIQUE NOT NULL,
   FOREIGN KEY (Name_tournament) REFERENCES Tournament(Name_tournament)
 ) COMMENT='Table storing viewership information across platforms.';
-
-INSERT INTO Peek_viewership (Name_tournament, Peek_viewers, View_english_streams, View_twitch_streams, Average_viewers, Hours_watched)
-VALUES 
-('League of legends: Worlds championship 2022', 5 147 701, 1 634 349, 2 873 711, 987 437, 141 943 967);
