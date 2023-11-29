6a3a60b55c660cd5f3077261d8a7f0de7f0663d5
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 11:53:49 +0700

    Update D.Teams_information.sql

--- D.Teams_information.sql
+++ D.Teams_information.sql
@@ -5,29 +5,3 @@ CREATE TABLE Teams_information (
   AREA VARCHAR(50) NOT NULL,
   Country VARCHAR(50) NOT NULL
 ) COMMENT='Table storing information about teams.';
-INSERT INTO Teams_information(Team_ID, Name_Team, Short_Name, AREA, Country)
-VALUE 
-(1, 'Top Esports', 'TES', 'LPL', 'China'), 
-(2, 'JD Gaming', 'JDG', 'LPL', 'China'),
-(3, 'EDward Gaming', 'EDG', 'LPL', 'China'),
-(4, 'Royal Never Give Up', 'RNG', 'LPL', 'China'),
-(5, 'Telecom 1', 'T1', 'LCK', 'Korea'),
-(6, 'Gen.G Esports', 'GenG', 'LCK', 'Korea'),
-(7, 'DWG KIA', 'DK', 'LCK', 'Korea'),
-(8, 'Dragon X', 'DRX', 'LCK', 'Korea'),
-(9, 'G2 Esports', 'G2', 'LEC', 'Europe'),
-(10, 'Rogue', 'RGE', 'LEC', 'Europe'),
-(11, 'Fnatic', 'FNC', 'LEC', 'Europe'),
-(12, 'MAD Lion', 'MAD', 'LEC', 'Europe'),
-(13, '100 Thieves', '100', 'LCS', 'North America'),
-(14, 'Cloud9', 'C9', 'LCS', 'North America'),
-(15, 'Evil Geniuses', 'EG', 'LCS', 'North America'),
-(16, 'Saigon Buffalo', 'SGB', 'VCS', 'Vietnam'),
-(17, 'GAM Esports', 'GAM', 'VCS', 'Vietnam'),
-(18, 'CTBC Flying Oyster', 'CFO', 'PCS', 'Taiwan'),
-(19, 'Beyond Gaming', 'BYG', 'PCS', 'Taiwan'),
-(20, 'Isurus', 'ISR', 'LLA', 'Latin America'),
-(21, 'İstanbul Wildcats', 'IW', 'TCL', 'Turkey'),
-(22, 'LOUD', 'LOUD', 'CBLOL', 'Brazil'),
-(23, 'DetonatioN FocusMe', 'DFM', 'LJL', 'Japan'),
-(24, 'Chiefs Esports Club', 'CE', 'LCO', 'Australia');
