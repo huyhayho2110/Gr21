a50f2d358fea2c41c75d84dc45b3d076045a7046
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 11:39:24 +0700

    Update 8.Players_information.sql

--- 8.Players_information.sql
+++ 8.Players_information.sql
@@ -2,14 +2,14 @@ CREATE TABLE Players_information (
   Player_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   Name_team VARCHAR(50) NOT NULL,
   Name_player VARCHAR (255) NOT NULL,
-  Position_player ENUM('Top','Jungle','Middle','ADC','Support'),
+  Lane ENUM('Top','Jungle','Middle','ADC','Support'),
   Coach_ID INT UNSIGNED,
   FOREIGN KEY (Name_team) REFERENCES Teams_information(Name_team),
   FOREIGN KEY (Coach_ID) REFERENCES Coaches(Coach_ID) ON DELETE SET NULL,
   CONSTRAINT UQ_Team_Player UNIQUE (Player_ID, Name_player)
 ) COMMENT='Table storing information about players.';
 
-INSERT INTO Players_information(Name_player, Name_team, Position_player)
+INSERT INTO Players_information(Name_player, Name_team, Lane)
 VALUES 
   ('369','JD Gaming','Top'),
   ('Abbedagge','100 Thieves','Middle'),
