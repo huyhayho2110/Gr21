4e1557ba2390dd1045d891a1ff759ba1579deb12
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-29 09:47:37 +0700

    Update Teams_ranking.sql

--- Teams_ranking.sql
+++ Teams_ranking.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Teams_ranking (
   Ranking_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   Ranking_position INT NOT NULL,
-  Team_ID INT UNSIGNED,
+  Team_ID INT UNSIGNED UNIQUE,
   FOREIGN KEY (Team_ID) REFERENCES Teams_information(Team_ID),
   CHECK (Ranking_position >= 1)
 ) COMMENT='Table storing ranking information for teams.';
+
 
 INSERT INTO Teams_ranking (Ranking_position, Team_ID) VALUES
 (1,8),
