7097062d8eebd670262634e8ec318bd2a16e9b4d
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-11-30 00:26:49 +0700

    Update J.Matches.sql

--- J.Matches.sql
+++ J.Matches.sql
@@ -4,6 +4,7 @@ CREATE TABLE Matches (
   ID_team1 INT UNSIGNED,
   ID_team2 INT UNSIGNED,
   Location VARCHAR(255),
+  FOREIGN KEY (Location) REFERENCES Tournament(Location),
   FOREIGN KEY (ID_team1) REFERENCES Teams_information(Team_ID),
   FOREIGN KEY (ID_team2) REFERENCES Teams_information(Team_ID)
 ) COMMENT='Table storing information about matches.';
