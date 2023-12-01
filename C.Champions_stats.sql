931cef18f509b60faf10ede20b1a08dc70367adb
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-12-01 23:08:46 +0700

    Create C.Champions_stats.sql

---
+++ C.Champions_stats.sql
@@ -0,0 +1,21 @@
+CREATE TABLE stats_champion (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    champion VARCHAR(50) NOT NULL,
+    sum_total INT NOT NULL,
+    win_total INT NOT NULL,
+    lose_total INT NOT NULL,
+    winrate_total DECIMAL(5,2) NOT NULL,
+    pick_rate DECIMAL(5,2) NOT NULL,
+    sum_blue_side INT NULL,
+    win_blue_side INT NULL,
+    lose_blue_side INT NULL,
+    winrate_blue_side DECIMAL(5,2) NULL,
+    sum_red_side INT NULL,
+    win_red_side INT NULL,
+    lose_red_side INT NULL,
+    winrate_red_side DECIMAL(5,2) NULL,
+    sum_bans INT NOT NULL,
+    ban_rate DECIMAL(5,2) NOT NULL,
+    sum_pick_ban INT NOT NULL,
+    pick_ban_rate DECIMAL(5,2) NOT NULL
+);
