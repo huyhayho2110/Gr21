02ad87a338964058948d054cbceaf05cfe8da958
hadinhtuan183 <151992758+hadinhtuan183@users.noreply.github.com>
2023-12-02 15:32:25 +0700

    K.GameResults.sql

--- K.GameResults.sql
+++ K.GameResults.sql
@@ -3,8 +3,8 @@ CREATE TABLE Game_results (
   ID_match INT UNSIGNED,
   ID_team_winning INT UNSIGNED,
   ID_team_losing INT UNSIGNED,
-  Winning_score INT UNSIGNED NOT NULL,
-  Losing_score INT UNSIGNED NOT NULL,
+  Winning_score INT UNSIGNED,
+  Losing_score INT UNSIGNED,
   FOREIGN KEY (ID_match) REFERENCES Matches(ID_match),
   FOREIGN KEY (ID_team_winning) REFERENCES Teams_information(Team_ID),
   FOREIGN KEY (ID_team_losing) REFERENCES Teams_information(Team_ID)
