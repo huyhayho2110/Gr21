7911327b66ab709bd96d4eb98a5fd5e2e9ad5987
huyhayho2110 <138202199+huyhayho2110@users.noreply.github.com>
2023-12-02 23:09:55 +0700

    Update Matches_Knockout.sql

--- Matches_Knockout.sql
+++ Matches_Knockout.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Matches_Knockout  (
+CREATE TABLE Matches(
   ID_match INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   Date DATE,
   ID_team1 INT UNSIGNED,
   ID_team2 INT UNSIGNED,
   Location VARCHAR(255),
+  type_matches VARCHAR (255),
   FOREIGN KEY (Location) REFERENCES Tournament(Location),
   FOREIGN KEY (ID_team1) REFERENCES Teams_information(Team_ID),
   FOREIGN KEY (ID_team2) REFERENCES Teams_information(Team_ID)
 ) COMMENT='Table storing information about matches.';
-
